5cc403680273a6f05faec1f918368050245f738c
akhoury105 <69321754+akhoury105@users.noreply.github.com>
2020-12-09 16:01:51 -0500

    Adds basic raffle command to check raffle status.

--- raffle_StreamlabsSystem.py
+++ raffle_StreamlabsSystem.py
@@ -1,4 +1,4 @@
-import os
+import os, codecs, json
 
 ScriptName = 'Raffle'
 Website = 'twitch.tv/blueboxfromspace'
@@ -7,13 +7,37 @@ Creator = 'BlueBoxFromSpace'
 Version = '1.0.0'
 Command = '!raffle'
 
+path = os.path.dirname(__file__)
+settingsFile = os.path.join(path, 'setting.json')
+
+respHowToBuy = ''
+respWhatToWin = ''
+
 
 def Init():
+    loadSettings()
     return
 
 
 def ReloadSettings(jsonData):
+    loadSettings()
     return
+
+
+def loadSettings():
+    global respWhatToWin, respHowToBuy
+    try:
+        with codecs.open(settingsFile, encoding='utf-8-sig', mode='r') as file:
+            settings = json.loads(file, encoding='utf-8-sig')
+    except:
+        log("Error Loading Settings. Loaded Default Instead")
+        settings = {
+            "HowToBuy" : "Use Channel Points to buy entries into this month's Raffle!",
+            "WhatToWin" : "This months prize is TBD! The Drawing will be held TBD."
+        }
+    
+    respHowToBuy = settings['HowToBuy']
+    respWhatToWin = settings['WhatToWin']
 
 
 def Unload():
@@ -21,17 +45,31 @@ def Unload():
 
 
 def Execute(data):
-    if data.IsChatMessage() and data.GetParam(0) == Command:
-    # !raffle username adds user to a text file.
-        if data.GetParam(1)
+    if data.IsChatMessage() and data.GetParam(0).lower() == Command:
+        # !raffle tells viewers what the current raffle is for.
+        if data.GetParamCount() == 1:
+            send_message(respHowToBuy)
+            send_message(respWhatToWin)
+
+        # !raffle draw select random user from text file.
+
+        # !raffle username adds user to a text file.
     
-    # !raffle draw select random user from text file.
-    # !raffle tells viewers what the current raffle is for.
+    return
+
+
+def drawRaffle():
+    # generate random number for index
+    # use that number to get a user from our list
     return
 
 
 def addUserToTextFile():
-
+    # open the file
+    # check to see if its empty
+    # add new line if its not empty
+    # add new user to new line
+    return
 
 
 def Tick():
@@ -39,7 +77,7 @@ def Tick():
 
 
 def OpenReadMe():
-	os.startfile(readMeFile)
+	#os.startfile(readMeFile)
 	return
 
 
