f08ef9037561ba6ee3b8fe52d84a3d274a2863e5
akhoury105 <69321754+akhoury105@users.noreply.github.com>
2020-12-10 16:10:26 -0500

    Version 1.0.0 Commit

---
+++ settings.js
@@ -0,0 +1,8 @@
+﻿var settings = {
+  "Permission": "Caster",
+  "HowToBuy": "Use Channel Points to buy entries into this month's Raffle!",
+  "WhatToWin": "This month's prize is TBD! The Drawing will be held TBD.",
+  "WinMessage": "This raffle's winner is @{0}!",
+  "AddedMessage": "@{0} has been added to the list.",
+  "ErrorMessage": "Error Occured. Please check the logs"
+};
